5ef5e760647e0925a1cdf516c3a39b69575d0aed
ADERITO XAVIER <aderito_xavier@sapo.pt>
2019-02-23 21:32:58 +0000

    Map class

--- battling-knights/knights.py
+++ battling-knights/knights.py
@@ -55,17 +55,65 @@ If knight moves onto a tile with 2 items: Equip only the best one in the followi
 import numpy as np
 
 
+class Arena:
+
+  def __init__(self, row_size, col_size, fill):
+    self.arena = np.full((row_size, col_size), fill)
+
+  def add_knight(self, name, start_row, start_col):
+    self.arena[start_row, start_col] = name
+
+  
+  def __str__(self):
+    return str(self.arena)
+
+  def add_item(self, name, row, col, attack, defence):
+    pass
+
+
+
 class Knight:
-  RED = 'R'
-  BLUE = 'B'
-  GREEN = 'G'
-  YELLOW = 'Y'
+  
+  base_attack = 1
+  base_defense = 1
+
+  def __init__(self, name, start_row, start_col):
+
+    self.name = name
+    self.start_row = start_row
+    self.start_col = start_col
+    
+  def get_name(self):
+    return self.name
+
+  def set_current_position(self, current_row, current_col):
+    self.current_row = current_row
+    self.current_col = current_col
+
+  def get_starting_position(self):
+    return self.start_row, self.start_col
 
 
-arena = np.full((8, 8), '|_|')
-arena[0,0] = 'R' 
-arena[7,0] = 'B' 
-arena[7,7] = 'G' 
-arena[0,7] = 'Y' 
+
+R = Knight('R', 0, 0)
+B = Knight('B', 7, 0)
+G = Knight('G', 7, 7)
+Y = Knight('Y', 0, 7)
+
+# Name, Attack, Defence, Importance
+A = ['A', 2, 0, 1]
+D = ['D', 1, 0, 2]
+H = ['H', 0, 1, 3]
+M = ['M', 1, 1, 4]
+
+knights = [R, B, G, Y] 
+items = [A, D, H, M]
+
+arena = Arena(8, 8, '|_|')
+
+for knight in knights:
+  starting_row, starting_col = knight.get_starting_position()
+  arena.add_knight(knight.get_name(), starting_row, starting_col)
+
 
 print(arena)
