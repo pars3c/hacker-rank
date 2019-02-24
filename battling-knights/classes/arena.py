e3425dc58535af88d15029edc6a6cc1c534fc0b4
ADERITO XAVIER <aderito_xavier@sapo.pt>
2019-02-24 15:37:16 +0000

    knights are drowning and dying

--- battling-knights/classes/arena.py
+++ battling-knights/classes/arena.py
@@ -1,9 +1,10 @@
 import numpy as np
+from .knight import Knight
 
-class Arena:
+class Arena(Knight):
   knights = []
   items = []
-
+  drowned_or_dead_knights = []
   def __init__(self, row_size, col_size, fill):
     self.arena = np.full((row_size, col_size), fill)
 
@@ -30,23 +31,47 @@ class Arena:
       print("Please do type a name of aa item, not a knight or anything else")
       return
 
+
   def moving_knight(self, name, current_row, next_row, current_col, next_col, fill_previous):
     """ Moves the Knight according the name and its current location """
+
+    if name in self.drowned_or_dead_knights:
+      """ If this knight is dead or drowned skip command on moves.txt """
+      return
+    
+
     print(("Knight {} current row {} next row {} current col {} next col {}"
     .format(name, current_row, next_row, current_col, next_col)))
 
     # Check if Knight next tile is gonna throw him off the board (Drowning him)
     if (next_row == (-1) or next_row == (8) or next_col == (-1) or next_col == (8)):
+      """ If next tile is the end of the array drown knight """
+      self.drowned_or_dead_knights.append(name) 
       self.arena[current_row, current_col] = fill_previous
-      print("Knight {} drowned".format(name))
-    #print("Name:", name, "Current row:", current_row, "Next row:", next_row)
 
-    # Check if a Knight is is on the next tile
-    elif self.knights in self.arena[current_row, current_col]:
-      pass
+      print("Knight {} drowned".format(name))
+      return
+      #self.arena = np.insert(self.arena, 0 , values=['-', '-', '-', '-', '-', '-', '-', '(y)'], axis=0)
+      # TODO add another row or col, to indicate knight drowned like (------(y))
+      
+    #print("Name:", name, "Current row:", current_row, "Next row:", next_row)
+    
+    if self.arena[next_row, next_col] in self.knights:
+      """ If this knight is dead or drowned skip command on moves.txt """
+      # TODO if knight in the next tile and there is no item: fight him
+      self.arena[next_row, next_col] = np.core.defchararray.add(self.arena[next_row, next_col], name)
+      self.arena[current_row, current_col] = fill_previous
+ 
     else:
       self.arena[current_row, current_col] = fill_previous
       self.arena[next_row, next_col] = name
+    # Check if a Knight is is on the next tile
+
+
+    
+    
+    
+    
     
   def add_item(self, name, start_row, start_col):
     """ Add item to the board according its name and its position """
--- battling-knights/classes/knight.py
+++ battling-knights/classes/knight.py
@@ -1,14 +1,23 @@
 
-class Knight:
+from .items import Item
+
+class Knight(Item):
   
   base_attack = 1
   base_defence = 1
-
+  knight_stats = {}
   
   def __init__(self, name, start_row, start_col):
     self.name = name
     self.start_row = start_row
     self.start_col = start_col
+    self.knight_stats = {
+      "name": self.name,
+      "base_attack": 1,
+      "base_defense": 1,
+      "has_item": False,
+      "item": "",     
+      }
 
   def get_name(self):
     return self.name
@@ -16,8 +25,9 @@ class Knight:
   def add_item(self, name, attack, defence, importance):
     self.base_attack += attack
     self.base_defence += defence
-
-
+    self.knight_stats["has_item", "item", "base_attack", "base_defense"] = (True, name, 
+    self.base_attack, self.base_defence) 
+    
   def set_current_position(self, current_row, current_col):
     self.current_row = current_row
     self.current_col = current_col
